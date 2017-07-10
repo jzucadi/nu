e7a04a8de92f71d64bb6f5808443ac1ab3e66433
James <jameszaccardo@gmail.com>
2017-07-10 10:07:02 -0700

    fixed typo

--- pages/index.vue
+++ pages/index.vue
@@ -6,8 +6,104 @@
       <h2 class="subtitle">
         We've Found Your Rates
       </h2>
-      <div class="links">
+      <div id="wrapper">
+ 
+
+  <h2 class="card-group-heading"></h2>
+  <div class="card-container">
+    <article class="card assignment-card course-id-1">
+      <header class="card-header card-backgrund-color">
+        <h2>Rocket Mortgage</h2>
+      </header>
+      <section class="card-body">
+        <div class="card-assignment-icon"><img src="http://benzingafintechawards.com/wp-content/uploads/wsi-imageoptim-RocketMortgage.jpg" alt="Mountain View" style="width:180px;height:75px;"></div>
+        <div class="card-assignment-text">
+          Fast, Powerful and Completely Online.
         </div>
+      </section>
+      <footer class="card-footer">
+        <p><span class="card-footer-text"><a class="button btn" href="#">
+          See Rate
+        </a></span>
+        </p>
+      </footer>
+    </article>
+
+    <article class="card assignment-card course-id-1">
+      <header class="card-header card-backgrund-color">
+        <h2>Loan Depot, LLC</h2>
+      </header>
+      <section class="card-body">
+        <div class="card-assignment-icon"><img src="http://photos.prnewswire.com/prnfull/20120829/SF64465LOGO" alt="Mountain View" style="width:200px;height:50px;"></div>
+        <div class="card-assignment-text">
+          Get a free and no obligation rate quote in California.
+        </div>
+      </section>
+      <footer class="card-footer">
+        <p><span class="card-footer-text"><a class="button btn" href="#">
+          See Rate
+        </a></span></p>
+      </footer>
+    </article>
+    <article class="card assignment-card course-id-4">
+      <header class="card-header card-backgrund-color">
+        <h2>Quicken Loans</h2>
+      </header>
+      <section class="card-body">
+        <div class="card-assignment-icon"><img src="http://www.quickenloans.com/press-room/wp-content/uploads/2016/11/QuickenLoans-Logo-Horiz.png" alt="Mountain View" style="width:220px;height:30px;"></div>
+        <div class="card-assignment-text">
+         Refinance to a get a lower payment, cash back or a faster payoff.
+        </div>
+      </section>
+      <footer class="card-footer">
+        <p><span class="ml-checkmark card-footer-icon card-backgrund-color"></span> <span class="card-footer-text">Recommended <a class="button btn" href="#">
+          See Rate
+        </a></span>
+        </p>
+      </footer>
+    </article>
+     <article class="card assignment-card course-id-1">
+      <header class="card-header card-backgrund-color">
+        <h2>Amerisave NMLS #1168</h2>
+      </header>
+      <section class="card-body">
+        <div class="card-assignment-icon"><img src="https://amerisave.hrmdirect.com/employment/filegen.php?orgId=enc-14.17785587711778&fn=b3JnX2xvZ28" alt="Mountain View" style="width:220px;height:47px;">
+</div>
+        <div class="card-assignment-text">
+          Direct Mortgage Lender. NMLS ID #1168.
+        </div>
+      </section>
+      <footer class="card-footer">
+        <p><span class="card-footer-text"><a class="button btn" href="#">
+          See Rate
+        </a></span>
+        </p>
+      </footer>
+    </article>
+      <article class="card assignment-card course-id-1">
+      <header class="card-header card-backgrund-color">
+        <h2>CloseYourOwnLoan.com</h2>
+      </header>
+      <section class="card-body">
+        <div class="card-assignment-icon"><img src="https://s3.consumersadvocate.org/prd/image/image/1294/medium_kjXhiFBPBJUInEHqXPZDeeShdnjWgu.gif" alt="Mountain View" style="width:220px;height:70px;">
+</div>
+        <div class="card-assignment-text">
+          $1,000 Best Rate Guarantee
+        </div>
+      </section>
+      <footer class="card-footer">
+        <p><span class="card-footer-text"><a class="button btn" href="#">
+          See Rate
+        </a></span>
+        </p>
+      </footer>
+    </article>
+    
+    
+  </div>
+  
+</div>
+
        
       </div>
     </div>
